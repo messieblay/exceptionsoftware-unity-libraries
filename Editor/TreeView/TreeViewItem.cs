cac3701832c43f23f9c004c46496f5aaa5615153
RECODOM\marco.blay <marco.alain@recotech.es>
2021-05-11 11:23:21 +0200

    * TreeViewElement

---
+++ Editor/TreeView/TreeViewItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor.IMGUI.Controls;
+
+namespace ExceptionSoftware.TreeViewTemplate
+{
+
+    public class TreeViewItem<T> : TreeViewItem where T : TreeElement
+    {
+        public T data { get; set; }
+
+        public TreeViewItem(int id, int depth, string displayName, T data) : base(id, depth, displayName)
+        {
+            this.data = data;
+        }
+    }
+}
