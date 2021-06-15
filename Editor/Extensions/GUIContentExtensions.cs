853faa76022608aa29a05b392e035dd7b26ccba5
RECODOM\marco.blay <marco.alain@recotech.es>
2021-06-15 12:19:11 +0200

    + GUIContent extensions

---
+++ Editor/Extensions/GUIContentExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public static class GUIContentExtensions
+{
+    public static GUIContent SetTooltip(this GUIContent value, string tooltip)
+    {
+        value.tooltip = tooltip;
+        return value;
+    }
+
+    public static GUIContent SetImage(this GUIContent value, Texture image)
+    {
+        value.image = image;
+        return value;
+    }
+
+    public static GUIContent SetText(this GUIContent value, string text)
+    {
+        value.text = text;
+        return value;
+    }
+
+}
