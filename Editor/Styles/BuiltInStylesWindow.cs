922b61f81907754a3878898efaf40cf32682d511
RECODOM\marco.blay <marco.alain@recotech.es>
2021-06-15 11:23:22 +0200

    * All Built in icons

--- Editor/Styles/BuiltInStylesWindow.cs
+++ Editor/Styles/BuiltInStylesWindow.cs
@@ -26,7 +26,7 @@ namespace ExceptionSoftware.ExEditor
 
         enum BultInState
         {
-            Styles, Icons, Editor
+            Styles, Icons, Editor, IconBundle
         }
 
         private List<Drawing> _drawings = new List<Drawing>();
@@ -129,6 +129,11 @@ namespace ExceptionSoftware.ExEditor
                     case BultInState.Editor:
                         CollectFromEditorStyles();
                         break;
+                    case BultInState.IconBundle:
+                        CollectIconsBundle(_search);
+                        break;
+
+
                 }
                 _isDataDirty = false;
                 //_maxY = y;
@@ -348,5 +353,134 @@ namespace ExceptionSoftware.ExEditor
                 _isDataDirty = true;
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+        #region IconsBundle
+        void CollectIconsBundle(string search)
+        {
+            _textures = GetListIconsbundle(search);
+
+            //Vector2 pos = Vector2.zero;
+            float x = 10, y = 0;
+            float rowHeight = 0.0f;
+
+            foreach (UnityEngine.Object oo in _textures)
+            {
+                Texture2D texture = (Texture2D)oo;
+
+                if (texture.name == "")
+                    continue;
+
+                if (search != "" && !texture.name.ToLower().Contains(search))
+                    continue;
+
+                if (texture.texelSize.x >= 512 || texture.texelSize.y >= 512)
+                {
+                    continue;
+                }
+
+                Drawing draw = new Drawing();
+
+                float width = Mathf.Max(GUI.skin.button.CalcSize(new GUIContent(texture.name)).x, texture.width) + 8.0f;
+                float height = texture.height + GUI.skin.button.CalcSize(new GUIContent(texture.name)).y + 8.0f;
+
+                if (x + width > position.width - 32.0f)
+                {
+                    x = 5.0f;
+                    y += rowHeight + 8.0f;
+                    rowHeight = 0.0f;
+                }
+                draw.Rect = new Rect(x, y, width, height);
+
+#if UNITY_2020_1_OR_NEWER
+                draw.copyText = "EditorGUIUtility.IconContent( \"" + texture.name + "\" ).icon";
+#else
+                draw.copyText = "EditorGUIUtility.FindTexture( \"" + texture.name + "\" )\n" + AssetDatabase.GetAssetPath(texture);
+#endif
+
+                draw.title = texture.name;
+
+                rowHeight = Mathf.Max(rowHeight, height);
+
+                width -= 8.0f;
+
+                draw.Draw = () =>
+                {
+                    Rect textureRect = GUILayoutUtility.GetRect(texture.width, texture.width, texture.height, texture.height, GUILayout.ExpandHeight(false), GUILayout.ExpandWidth(false));
+                    EditorGUI.DrawTextureTransparent(textureRect, texture);
+                };
+
+                x += width + 8.0f;
+                _drawings.Add(draw);
+            }
+
+            _maxY = _drawings.Last().Rect.yMax;
+        }
+
+        private static List<Texture2D> GetListIconsbundle(string filter)
+        {
+            List<Texture2D> list = new List<Texture2D>();
+            var editorAssetBundle = GetEditorAssetBundle();
+            var iconsPath = GetIconsPath();
+            var count = 0;
+            foreach (var assetName in EnumerateIcons(editorAssetBundle, iconsPath, filter))
+            {
+                var icon = editorAssetBundle.LoadAsset<Texture2D>(assetName);
+                if (icon == null)
+                    continue;
+
+                list.Add(icon);
+            }
+            return list;
+        }
+        private static IEnumerable<string> EnumerateIcons(AssetBundle editorAssetBundle, string iconsPath, string filter)
+        {
+            foreach (var assetName in editorAssetBundle.GetAllAssetNames())
+            {
+
+                if (assetName.StartsWith(iconsPath, StringComparison.OrdinalIgnoreCase) == false)
+                    continue;
+                if (assetName.EndsWith(".png", StringComparison.OrdinalIgnoreCase) == false &&
+                    assetName.EndsWith(".asset", StringComparison.OrdinalIgnoreCase) == false)
+                    continue;
+                if (!filter.IsNullOrEmpty() && !assetName.Contains(filter)) continue;
+                yield return assetName;
+            }
+        }
+        private static AssetBundle GetEditorAssetBundle()
+        {
+            var editorGUIUtility = typeof(EditorGUIUtility);
+            var getEditorAssetBundle = editorGUIUtility.GetMethod(
+                "GetEditorAssetBundle",
+                BindingFlags.NonPublic | BindingFlags.Static);
+
+            return (AssetBundle)getEditorAssetBundle.Invoke(null, new object[] { });
+        }
+
+        private static string GetIconsPath()
+        {
+#if UNITY_2018_3_OR_NEWER
+            return UnityEditor.Experimental.EditorResources.iconsPath;
+#else
+            var assembly = typeof(EditorGUIUtility).Assembly;
+            var editorResourcesUtility = assembly.GetType("UnityEditorInternal.EditorResourcesUtility");
+
+            var iconsPathProperty = editorResourcesUtility.GetProperty(
+                "iconsPath",
+                BindingFlags.Static | BindingFlags.Public);
+
+            return (string)iconsPathProperty.GetValue(null, new object[] { });
+#endif
+        }
+        #endregion
     }
 }
