ecd7546f30da1e14cfd1b2d279ac9519c8ec1b71
RECODOM\marco.blay <marco.alain@recotech.es>
2021-07-23 15:18:58 +0200

    * To Sentence

---
+++ Runtime/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+
+public static class StringExtensions
+{
+    public static string ToSentence(this string str) => string.Concat(str.Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+
+}
